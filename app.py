f90f2cf957c590f6ea92bd97ec38d7b7d43f9f15
y-program-tho <yanga12edu@gmail.com>
2026-08-06 23:41:10 +0100

    add technolgies to README

--- app.py
+++ app.py
@@ -3,7 +3,31 @@ import streamlit as st
 
 st.title('Welcome to y_footy_analytics_v1')
 
+st.write('')
+st.write('')
+st.write('')
+
 st.dataframe(pd.DataFrame({
     'col1': ['a', 'b', 'c', 'd'],
     'col2': [1, 2, 3, 4]
 }))
+
+st.write('Viz1')
+
+st.write('')
+st.write('')
+st.write('')
+
+st.write('Viz2')
+
+st.write('')
+st.write('')
+st.write('')
+
+st.write('Viz3')
+
+st.write('')
+st.write('')
+st.write('')
+
+st.write('Viz4')
